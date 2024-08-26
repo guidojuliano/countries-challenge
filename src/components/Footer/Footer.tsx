76ec86e17e37f5771c93298fe764e1a3273771f8
Guido Juliano Ubaldi <guidojulianou@gmail.com>
2024-08-26 12:49:24 -0300

    Se agrega link a mi portfolio en el footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,11 @@ export const Footer = () => {
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2024{" "}
-          <a href="/" className="hover:underline">
+          <a
+            href="https://www.guidojuliano.site/"
+            target="_blank"
+            className="hover:underline"
+          >
             Guido Juliano Ubaldi
           </a>
         </span>
